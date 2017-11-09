9519302c66fe5fd062be79fdd7b88a090b21734c
camkirk <camden.kirkland@gmail.com>
2017-11-09 11:01:46 -0600

    create harvest method in main.js and need to update post route in apiroutes

--- public/main.js
+++ public/main.js
@@ -1,5 +1,5 @@
 var cityMap = [
-  {  
+  {
      address: "Houston, TX",
      population: 2714856
    },
@@ -17,14 +17,33 @@ var cityMap = [
    }
  ];
 
- function initMap() {
+function initMap(data) {
    var myLatLng = {lat:37.697948 , lng: -97.314835};
    var map = new google.maps.Map(document.getElementById('map'), {
      zoom: 5,
      center: myLatLng
    });
 
+   if(data){
+     data.forEach(function(city){
+        var geocoder = new google.maps.Geocoder();
+        geocoder.geocode({'address':city.address}, function(results, status) {
+          var cityCircle = new google.maps.Circle({
+            strokeColor: '#FF0000',
+            strokeOpacity: 0.8,
+            strokeWeight: 2,
+            fillColor: '#FF0000',
+            fillOpacity: 0.35,
+            map: map,
+            center: {lat: results[0].geometry.location.lat(), lng: results[0].geometry.location.lng()},
+            radius: Math.sqrt(city.population) * 100
+          });
+        });
+      });
+
+   }
    // Add the circle for this city to the map.
+   else{
     cityMap.forEach(function(city){
        var geocoder = new google.maps.Geocoder();
        geocoder.geocode({'address':city.address}, function(results, status) {
@@ -38,11 +57,25 @@ var cityMap = [
            center: {lat: results[0].geometry.location.lat(), lng: results[0].geometry.location.lng()},
            radius: Math.sqrt(city.population) * 100
          });
-       });  
-     }); 
+       });
+     });
+   }
  }  //init map
- 
+
+function harvest(){
+  var searchterm = $('#newKeywords1').val() || "Javascript";
+
+  $.ajax({
+    url: "/api/",
+    method: "POST",
+    data:searchterm
+  }).done(function(data){
+      initMap(data);
+  });
+
+}
+
  $( document ).ready(function() {
   console.log( "ready!" );
-  initMap();
+  harvest();
 });
